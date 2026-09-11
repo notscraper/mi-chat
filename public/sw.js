f90cf39bd07ec248bb11d2f854e12874dfa921ff
notscraper <notscraper@gmail.com>
2026-09-11 15:50:29 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Nuevo mensaje';
+  const options = {
+    body: data.body || 'Tienes un nuevo mensaje en el chat.',
+    icon: 'https://cdn-icons-png.flaticon.com/512/134/134937.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/134/134937.png',
+    vibrate: [100, 50, 100]
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
